27686c8dc7cbd677829a055e9aa22538b8c315aa
AndersRo <anroqui2011@gmail.com>
2018-11-26 00:11:25 -0500

    update

--- application/controllers/Talleres.php
+++ application/controllers/Talleres.php
@@ -1,18 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Mecanicos extends My_Controller {
+class Talleres extends My_Controller {
 
   public function __construct()
 	{
 		  parent::__construct();
-      $this->load->model('MecanicosModel');
+      $this->load->model('TalleresModel');
 	}
 
 	 public function index()
  	{
- 		$data = array('titulo' => 'Mecanicos','token'  => $this->auth->token());
- 		$this->layout('mecanicos/index_view',$data);
+ 		$data = array('titulo' => 'Talleres','token'  => $this->auth->token());
+ 		$this->layout('talleres/index_view',$data);
   }
 
 
@@ -25,10 +25,10 @@ class Mecanicos extends My_Controller {
           $sord =$_GET['sord'];
           $atrr=$this->grilla->param;
           //Obteniendo el Count
-          $datacount=$this->MecanicosModel->Listar($sidx,$sord,$atrr,$this->auth->getidempresa()); //count
+          $datacount=$this->TalleresModel->Listar($sidx,$sord,$atrr); //count
           $atrr=$this->grilla->jq_getatributes($this->input->get(),$datacount);
           //Obteniendo la Data
-          $data=$this->MecanicosModel->Listar($sidx,$sord,$atrr,$this->auth->getidempresa()); //data
+          $data=$this->TalleresModel->Listar($sidx,$sord,$atrr); //data
           $json=$this->grilla->jq_getdata($this->input->get(),$data);
           echo $json;
       }
--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -1,18 +1,18 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Mecanicos extends My_Controller {
+class Usuarios extends My_Controller {
 
   public function __construct()
 	{
 		  parent::__construct();
-      $this->load->model('MecanicosModel');
+      $this->load->model('UsuariosModel');
 	}
 
 	 public function index()
  	{
- 		$data = array('titulo' => 'Mecanicos','token'  => $this->auth->token());
- 		$this->layout('mecanicos/index_view',$data);
+ 		$data = array('titulo' => 'Usuarios','token'  => $this->auth->token());
+ 		$this->layout('usuarios/index_view',$data);
   }
 
 
@@ -25,10 +25,10 @@ class Mecanicos extends My_Controller {
           $sord =$_GET['sord'];
           $atrr=$this->grilla->param;
           //Obteniendo el Count
-          $datacount=$this->MecanicosModel->Listar($sidx,$sord,$atrr,$this->auth->getidempresa()); //count
+          $datacount=$this->UsuariosModel->Listar($sidx,$sord,$atrr,$this->auth->getidempresa()); //count
           $atrr=$this->grilla->jq_getatributes($this->input->get(),$datacount);
           //Obteniendo la Data
-          $data=$this->MecanicosModel->Listar($sidx,$sord,$atrr,$this->auth->getidempresa()); //data
+          $data=$this->UsuariosModel->Listar($sidx,$sord,$atrr,$this->auth->getidempresa()); //data
           $json=$this->grilla->jq_getdata($this->input->get(),$data);
           echo $json;
       }
